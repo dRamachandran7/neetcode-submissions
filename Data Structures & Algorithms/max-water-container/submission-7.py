d3be938651430d791480ae435f1f61a94e963365
Dhruva Ramachandran <dhruva.ramachandran@gmail.com>
2026-07-16 16:19:07 -0700

    Add: max-water-container - submission-7

---
+++ Data Structures & Algorithms/max-water-container/submission-7.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        # Width of container is j - i where i, j are the indices of
+        # the bar. height is min(i,j). If we use standard two pointer
+        # approach, width is already maximized.
+
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while (l < r):
+            smaller = heights[l] if heights[l] < heights[r] else heights[r]
+            p = smaller * (r - l)
+
+            if p > res:
+                res = p
+            if heights[r] < heights[l]:
+                r -= 1
+            else:
+                l += 1
+        
+        return res
+
+
+
+
+
+        
